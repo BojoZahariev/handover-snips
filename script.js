6e4081bc3d293bf079417f25d23f77d2222eb7bf
Bojo <bozhidar_zahariev@hotmail.com>
2020-02-05 07:50:26 +0000

    newBtn

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-//USE for reception
+const initialDiv = document.querySelector('#initialDiv');
+const newBtn = document.querySelector('#newBtn');
 const submitBtn = document.querySelector('#submitBtn');
 const colleaguesForm = document.querySelector('#colleaguesForm');
 const input = document.querySelector('#input1');
 const test = document.querySelector('#test');
+const newForm = document.querySelector('#new');
 
-colleaguesForm.addEventListener('submit', function(e) {
+//USE for reception
+colleaguesForm.addEventListener('submit', e => {
   e.preventDefault();
-  test.textContent = input.value;
-  colleaguesForm.reset();
+  if (input.value.length > 1) {
+    test.textContent = input.value;
+    colleaguesForm.reset();
+  } else {
+    console.log('ding');
+    colleaguesForm.reset();
+  }
+});
+
+newBtn.addEventListener('click', e => {
+  initialDiv.style.display = 'none';
+  newForm.style.display = 'block';
 });
